3267e0c2e815629b3ef16180471cdb39a8a6d55a
AbdulAlt <lancerdeltarune8@gmail.com>
2024-10-20 19:43:12 +0100

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+# Libraries
+import pgzrun
+import pygame
+
+# Draw Function
+def draw():
+    screen.clear()
+    screen.draw.text('Welcome to game dev 1!', (100, 100), color = 'white')
+
+pgzrun.go()
