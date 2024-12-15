2fb04e5d4686c64a43fa0925a404d1924c6cb69c
AbdulAlt <lancerdeltarune8@gmail.com>
2024-12-15 19:55:41 +0000

    Update main.py

--- goodshot/main.py
+++ goodshot/main.py
@@ -1,51 +1,64 @@
 # importing libraries
 import pgzrun
-import random
+from random import randint
+from time import time
 
-# output properties
-TITLE = 'Shoot The Alien'
-WIDTH = 500
-HEIGHT = 500
+# settings
+TITLE = 'Game'
+WIDTH = 800
+HIEGHT = 600
 
-# initializing message, alien, and score
-message = 'shoot it.'
-alien = Actor('alien')
-score = 0
+# variables
+sl = []
+lines = []
+next_sat = 0
+num_of_sats = 8
 
-# pop function that plays the pop sound
-def pop_sound():
-    music.play('song')
+start_time = 0
+total_time = 0
+end_time = 0
+# create sats
+def create_sats():
+    global start_time
+    for i in range(8):
+        sats = Actor('spr_sat')
+        sats.pos = (randint(40, WIDTH - 40), randint(40, HIEGHT - 40))
+        sl.append(sats)
+        start_time = time()
 
-# song function that plays the song
-def song():
+# draw function
+def draw():
+    global total_time
+    # draw background
+    screen.blit('spr_bg', (0, 0))
+    # number of sats
+    num = 1
+    for i in sl:
+        screen.draw.text(str(num), (i.pos[0], i.pos[1] + 20))
+        i.draw()
+        num += 1
+
+    for i in lines:
+        screen.draw.line(i[0], i[1], (255, 255, 255))
+        if next_sat < num_of_sats:
+            total_time = time() - start_time
+            screen.draw.text(str(time), (10, 10), fontsize = 30)
+        else:
+            screen.draw.text(str(time), (10, 10), fontsize = 30)
+    def on_mouse_down(pos):
+        global next_sat, lines
+        if next_sat < num_of_sats:
+            if sl[next_sat].collidepoint(pos):
+                if next_sat:
+                    lines.append(sl[next_sat - 1].pos, sl[next_sat.pos])
+                    next_sat += 1
+                else:
+                    lines = []
+                    next_sat = 0
+def update():
     pass
 
-# draw function to render the game scene
-def draw():
-    screen.clear()
-    screen.fill(color=(0, 0, 200))  # Set background color
-    alien.draw()  # Draw the alien at the center
-    screen.draw.text(message, center=(250, 10), fontsize=30)  # Display the score
 
-# place alien in the center of the screen
-def place_alien():
-    alien.x = random.randint(50, WIDTH - 50)
-    alien.y = random.randint(50, HEIGHT - 50)
-
-# on_mouse_down function to handle mouse clicks
-def on_mouse_down(pos):
-    global message
-    global score
-    if alien.collidepoint(pos):  # Check if the click is on the alien
-        score += 1  # Increment the score
-        message = f'score: {score}'
-        pop_sound()
-        place_alien()  # Keep the alien in the center
-    else:
-        score -= 1  # Decrement the score
-        message = f'score: {score}'
-
-# Stuff
-song()
-place_alien()
+# calling functions
+create_sats()
 pgzrun.go()
